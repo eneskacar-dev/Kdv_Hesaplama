979f38df9e23e1bf59a22ef3c44423b5081e34e6
Enes Kaçar <156174705+eneskacar-dev@users.noreply.github.com>
2024-02-20 07:26:10 +0300

    KDV hesaplama

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,18 @@
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
+        double tutar , kdvOran = 0.18, kdvTutar, kdvliTutar ;
+                Scanner input = new Scanner(System.in);
+        System.out.print("Ücret Tutarını Giriniz :  ");
+        tutar = input.nextDouble();
+         kdvTutar = tutar * kdvOran ;
+         kdvliTutar = tutar + kdvTutar;
 
-        // Değişkenleri oluştur
-        int mat , fizik , kimya , turkce , muzik;
+        System.out.println("KDV'siz Tutar :" + tutar);
+        System.out.println("KDV Oranı :" + kdvOran);
+        System.out.println("KDV Tutarı :" + kdvliTutar);
+        System.out.println("KDV'li Tutar :" + kdvliTutar);
 
-        // Scanner sınıfımızı tanımladık
-    Scanner input = new Scanner(System.in);
 
-    //Kullanıcıdan değerleri alacağız.
-        System.out.print("Matematik Notunuz :  ");
-        mat = input.nextInt();
-
-        System.out.print("Fizik Notunuz :  ");
-        fizik = input.nextInt();
-
-        System.out.print("Kimya Notunuz :  ");
-        kimya = input.nextInt();
-
-        System.out.print("Türkçe Notunuz :  ");
-        turkce = input.nextInt();
-
-        System.out.print("Müzik Notunuz :  ");
-        muzik =input.nextInt();
-// Notların ortalamasını hesapla
-int toplam = (mat + fizik + kimya + muzik + turkce);
-double sonuc = toplam / 6 ;
-  String durum = (sonuc >= 60) ? "Sınıfı Geçti" : "Sınıfta Kaldı" ;
-
-System.out.println(durum);
     }
 }
